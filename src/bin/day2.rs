55e9a29e6d7ba7af8b93268c9d2f3663d00ed7c8
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-12-02 08:39:10 +0100

    Part1

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,11 +1,17 @@
 use std::fs;
 
 fn part1(input: &str) -> u32 {
-    todo!()
+    let games = parse_input(input);
+
+    games
+        .iter()
+        .filter(|game| game.is_possible(12, 13, 14))
+        .map(|game| game.id)
+        .sum()
 }
 
 fn part2(input: &str) -> u32 {
-    todo!()
+    0
 }
 
 fn main() {
@@ -15,21 +21,117 @@ fn main() {
     println!("Answer to day2 part 2: {}", part2(&input));
 }
 
+fn parse_input(input: &str) -> Vec<Game> {
+    input.lines().map(Game::parse).collect()
+}
+
+#[derive(Debug, PartialEq)]
+struct Game {
+    id: u32,
+    cubes_revealed: Vec<CubesRevealed>,
+}
+
+impl Game {
+    pub fn parse(input: &str) -> Game {
+        let mut parts = input.split(":");
+        let id = parts
+            .next()
+            .unwrap()
+            .strip_prefix("Game ")
+            .unwrap()
+            .trim()
+            .parse()
+            .unwrap();
+        let cubes_revealed = parts
+            .next()
+            .unwrap()
+            .split(";")
+            .map(CubesRevealed::parse)
+            .collect();
+
+        Game { id, cubes_revealed }
+    }
+
+    pub fn is_possible(&self, red: u32, green: u32, blue: u32) -> bool {
+        self.cubes_revealed
+            .iter()
+            .all(|cr| cr.red <= red && cr.green <= green && cr.blue <= blue)
+    }
+}
+
+#[derive(Default, Debug, PartialEq)]
+struct CubesRevealed {
+    blue: u32,
+    green: u32,
+    red: u32,
+}
+
+impl CubesRevealed {
+    pub fn parse(input: &str) -> CubesRevealed {
+        let mut result = CubesRevealed::default();
+        for extraction in input.split(",") {
+            let mut parts = extraction.trim().splitn(2, " ");
+            let count: u32 = parts.next().unwrap().trim().parse().unwrap();
+            let color = parts.next().unwrap().trim();
+            match color {
+                "blue" => result.blue += count,
+                "red" => result.red += count,
+                "green" => result.green += count,
+                _ => panic!(),
+            }
+        }
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_part_1() {
-        let example_input = r#""#;
+    fn test_parse_game() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
 
-        assert_eq!(142, part1(example_input));
+        assert_eq!(
+            Game {
+                id: 1,
+                cubes_revealed: vec![
+                    CubesRevealed {
+                        blue: 3,
+                        red: 4,
+                        green: 0
+                    },
+                    CubesRevealed {
+                        red: 1,
+                        green: 2,
+                        blue: 6
+                    },
+                    CubesRevealed {
+                        green: 2,
+                        red: 0,
+                        blue: 0
+                    }
+                ]
+            },
+            Game::parse(input)
+        );
     }
 
     #[test]
-    fn test_part_2() {
-        let example_input = r#""#;
+    fn test_part_1() {
+        let example_input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
 
-        assert_eq!(281, part2(example_input));
+        assert_eq!(8, part1(example_input));
     }
+
+    // #[test]
+    // fn test_part_2() {
+    //     let example_input = r#""#;
+
+    //     assert_eq!(281, part2(example_input));
+    // }
 }
