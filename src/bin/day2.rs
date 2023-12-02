b29f4157351b364d061e3a6effc097b7ec15cc10
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2023-12-02 08:46:24 +0100

    Part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -11,7 +11,13 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    0
+    let games = parse_input(input);
+
+    games
+        .iter()
+        .map(|game| game.min_set_of_cubes())
+        .map(|cubes| cubes.power())
+        .sum()
 }
 
 fn main() {
@@ -28,7 +34,7 @@ fn parse_input(input: &str) -> Vec<Game> {
 #[derive(Debug, PartialEq)]
 struct Game {
     id: u32,
-    cubes_revealed: Vec<CubesRevealed>,
+    cubes_revealed: Vec<Cubes>,
 }
 
 impl Game {
@@ -42,12 +48,7 @@ impl Game {
             .trim()
             .parse()
             .unwrap();
-        let cubes_revealed = parts
-            .next()
-            .unwrap()
-            .split(";")
-            .map(CubesRevealed::parse)
-            .collect();
+        let cubes_revealed = parts.next().unwrap().split(";").map(Cubes::parse).collect();
 
         Game { id, cubes_revealed }
     }
@@ -57,18 +58,26 @@ impl Game {
             .iter()
             .all(|cr| cr.red <= red && cr.green <= green && cr.blue <= blue)
     }
+
+    pub fn min_set_of_cubes(&self) -> Cubes {
+        Cubes {
+            blue: self.cubes_revealed.iter().map(|cr| cr.blue).max().unwrap(),
+            green: self.cubes_revealed.iter().map(|cr| cr.green).max().unwrap(),
+            red: self.cubes_revealed.iter().map(|cr| cr.red).max().unwrap(),
+        }
+    }
 }
 
 #[derive(Default, Debug, PartialEq)]
-struct CubesRevealed {
+struct Cubes {
     blue: u32,
     green: u32,
     red: u32,
 }
 
-impl CubesRevealed {
-    pub fn parse(input: &str) -> CubesRevealed {
-        let mut result = CubesRevealed::default();
+impl Cubes {
+    pub fn parse(input: &str) -> Cubes {
+        let mut result = Cubes::default();
         for extraction in input.split(",") {
             let mut parts = extraction.trim().splitn(2, " ");
             let count: u32 = parts.next().unwrap().trim().parse().unwrap();
@@ -81,6 +90,10 @@ impl CubesRevealed {
             }
         }
         result
+    }
+
+    pub fn power(&self) -> u32 {
+        self.red * self.green * self.blue
     }
 }
 
@@ -96,17 +109,17 @@ mod tests {
             Game {
                 id: 1,
                 cubes_revealed: vec![
-                    CubesRevealed {
+                    Cubes {
                         blue: 3,
                         red: 4,
                         green: 0
                     },
-                    CubesRevealed {
+                    Cubes {
                         red: 1,
                         green: 2,
                         blue: 6
                     },
-                    CubesRevealed {
+                    Cubes {
                         green: 2,
                         red: 0,
                         blue: 0
@@ -128,10 +141,14 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
         assert_eq!(8, part1(example_input));
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     let example_input = r#""#;
+    #[test]
+    fn test_part_2() {
+        let example_input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
 
-    //     assert_eq!(281, part2(example_input));
-    // }
+        assert_eq!(2286, part2(example_input));
+    }
 }
